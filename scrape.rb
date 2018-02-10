13ea60f39980c164883c5c853665029423350566
atsuminn <a23.ma2zaka@gmail.com>
2018-02-10 21:24:24 +0900

    update

---
+++ scrape.rb
@@ -0,0 +1,22 @@
+require 'nokogiri'
+require 'open-uri'
+# ssl証明を任せる
+require 'certified'
+
+# エンコード状況確認。"で囲むと中に式を記述可能
+p "Now Encoding : #{__ENCODING__}"  
+
+url = 'https://qiita.com/search?q=ruby'
+
+charset = nil
+
+html = open(url) do |f|
+    charset = f.charset
+    p "Sites Encoding : #{charset}" # サイトの文字コードを確認
+    f.read
+end
+
+doc = Nokogiri::HTML.parse(html, nil, charset)
+doc.xpath('//h1[@class="searchResult_itemTitle"]').each do |node|
+  p node.css('a').inner_text
+end
